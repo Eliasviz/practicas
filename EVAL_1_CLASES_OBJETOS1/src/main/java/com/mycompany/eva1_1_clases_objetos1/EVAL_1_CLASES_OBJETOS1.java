0c6ae6c686f123b807dc04f492a59f61f838d7d6
Eliasviz <158387644+Eliasviz@users.noreply.github.com>
2024-02-19 22:01:21 -0600

    Add files via upload

---
+++ EVAL_1_CLASES_OBJETOS1/src/main/java/com/mycompany/eva1_1_clases_objetos1/EVAL_1_CLASES_OBJETOS1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_clases_objetos1;
+import java.util.Scanner;
+/**
+ *
+ * @author Elias
+ */
+public class EVAL_1_CLASES_OBJETOS1 {
+
+    public static void main(String[] args) {
+        
+     int valor = 100;
+     persona perso1 = new Scanner (System.in);
+        System.out.println("perso 1");
+    }
+class persona{
+    }
+}
